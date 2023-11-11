6cbe21f01b4df05d4e9e8840d44ac5609545e2fc
GrinLOLZ <xenapsis@gmail.com>
2023-11-11 16:44:32 -0500

    test

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ var navbar = `
     <ul class="navcontainer">
             <img src="imgs/favicon.png" style=" width:50px; height: 50px;">
             <h1 class="name">Xenapsis</h1>
-            <li class="navlist" style="height: 15%;"><a href="index.html"><button class="navbutton">Home</button></a></li>
-            <li class="navlist" style="height: 15%;"><a href="games.html"><button class="navbutton">Games</button></a></li>
-            <li class="navlist" style="height: 15%;"><a href="apps.html"><button class="navbutton">Apps/Shows</button></a></li>
+            <li class="navlist" style="height: 15%;"><a href="index"><button class="navbutton">Home</button></a></li>
+            <li class="navlist" style="height: 15%;"><a href="games"><button class="navbutton">Games</button></a></li>
+            <li class="navlist" style="height: 15%;"><a href="apps"><button class="navbutton">Apps/Shows</button></a></li>
             <li class="navlist" style="height: 15%;"><button class="navbutton">Socials</button></li>
-            <li class="navlist" style="height: 15%;"><a href="settings.html"><button class="navbutton">Options</button></a></li>
-            <li class="navlist" style="height: 15%;"><a href="updates.html"><button class="navbutton">Updates</button></a></li>
+            <li class="navlist" style="height: 15%;"><a href="settings"><button class="navbutton">Options</button></a></li>
+            <li class="navlist" style="height: 15%;"><a href="updates"><button class="navbutton">Updates</button></a></li>
     </ul>
 </nav>
 `
