4176c2e4f270b1f63a0f3eb1b252d0586deaf8c0
GrinLOLZ <xenapsis@gmail.com>
2023-11-13 01:59:56 -0500

    footer

---
+++ js/lilfooter.js
@@ -0,0 +1,12 @@
+var footer = `
+<br>
+        <br>
+        <footer style="background-color: var(--lighter-color); display: flex; width: 100%; float: left;">
+            <ul style="display: flex; gap: 5rem; list-style: none;">
+                <li style="color: var(--offset-color);">Creator/Founder: Grin</li>
+                <li style="color: var(--offset-color);">Email Us: Xenapsis@gmail.com</li>
+                <li style="color: var(--offset-color);">Version 2.2.1</li>
+            </ul>
+        </footer>`;
+
+        document.querySelector("#footerthing").insertAdjacentHTML("beforeend", footer)
