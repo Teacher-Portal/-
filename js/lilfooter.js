0a945e01018299e95532eeba2fc13ed75787c452
Xenapsis <149744857+Xenapsis@users.noreply.github.com>
2023-12-06 14:07:02 -0500

    Update lilfooter.js

--- js/lilfooter.js
+++ js/lilfooter.js
@@ -8,9 +8,9 @@ var footer = `
             </div>
             <hr>
             <ul style="display: flex; gap: 5rem; list-style: none;">
-                <li style="color: var(--accent-color);">Creator/Founder: Grin</li>
-                <li style="color: var(--accent-color);">Email Us: Xenapsis@gmail.com</li>
-                <li style="color: var(--accent-color);">Version 2.8.8</li>
+                <li style="color: var(--offset-color);">Creator/Founder: Grin</li>
+                <li style="color: var(--offset-color);">Email Us: Xenapsis@gmail.com</li>
+                <li style="color: var(--offset-color);">Version 2.8.8</li>
             </ul>
         </footer>`;
 
